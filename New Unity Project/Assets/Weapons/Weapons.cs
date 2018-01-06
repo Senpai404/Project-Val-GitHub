b66390cbedca2347bdc5f3aa7a21f23d6b44e3b3
Senpai404 <Yaro.v.pante@gmail.com>
2018-01-06 14:16:20 +0100

    REuploading U-2017 version

---
+++ New Unity Project/Assets/Weapons/Weapons.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = ("RPG/Weapons"))]
+public class Weapons : ScriptableObject {
+
+    public Transform gripTransform;
+
+    [SerializeField] GameObject weaponPrefab;
+    [SerializeField] AnimationClip attackAnimation;
+
+    public GameObject GetWeaponPreFab()
+    {
+        return weaponPrefab;
+    }
+}
